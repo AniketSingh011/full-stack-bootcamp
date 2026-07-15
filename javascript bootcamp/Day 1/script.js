b11abc7f53f8c002c2f119f6f75fc1da2286b2f9
Aniket Singh <aniketksingh011@gmail.com>
2026-07-15 13:46:05 +0530

    Day 1 completed

---
+++ javascript bootcamp/Day 1/script.js
@@ -0,0 +1,9 @@
+let name = "Aniket";
+let age = 20;
+let college = "SICSR";
+let city = "pune";
+
+console.log("hello, my name is " + name);
+console.log("i am "+ age + " years old");
+console.log("i study in college " + college);
+console.log("i live in "+ city + " city");
